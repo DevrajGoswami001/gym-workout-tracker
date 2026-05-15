45111f5b4ce9185310e66d9cec234eac705a59f7
DevrajGoswami001 <162103958+DevrajGoswami001@users.noreply.github.com>
2026-05-15 17:03:39 +0530

    Added Android app with Capacitor

---
+++ android/app/src/main/java/com/devraj/gymtracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.devraj.gymtracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devraj.gymtracker',
+  appName: 'Gym Workout Tracker',
+  webDir: 'dist'
+};
+
+export default config;
